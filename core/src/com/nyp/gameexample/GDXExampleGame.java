f7c5c8fe691ed7b707ca174dfcfb76996fa81804
lilrosinante <60463014+lilrosinante@users.noreply.github.com>
2021-06-02 14:28:20 +0200

    added pictures, created Player, added Settings for sizes

--- core/src/com/nyp/gameexample/GDXExampleGame.java
+++ core/src/com/nyp/gameexample/GDXExampleGame.java
@@ -1,41 +1,38 @@
 package com.nyp.gameexample;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class GDXExampleGame extends ApplicationAdapter {
+public class GDXExampleGame extends Game {
 
+	private Player player;
+	private Texture character;
 	SpriteBatch batch;
-	Texture img;
-	private int x;
-	private int y;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		character = new Texture("Character/brendan_stand_south.png");
+		player = new Player(1, 1);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1f, 0f, 0f, 1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, x, y);
-		move();
+		batch.draw(character, player.getX()*Settings.SCALED_TILE_SIZE, player.getY()*Settings.SCALED_TILE_SIZE, Settings.SCALED_TILE_SIZE, Settings.SCALED_TILE_SIZE*1.5f);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		character.dispose();
 	}
 
-	public void move(){
-		x++;
-		y++;
-	}
 }
