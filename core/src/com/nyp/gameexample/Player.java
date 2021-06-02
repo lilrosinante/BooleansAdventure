a72d3e1d61087a7c71c198e9e9c856d14692a2bc
lilrosinante <60463014+lilrosinante@users.noreply.github.com>
2021-06-02 14:54:10 +0200

    fixed merge issue

--- core/src/com/nyp/gameexample/Player.java
+++ core/src/com/nyp/gameexample/Player.java
@@ -15,11 +15,11 @@ public class Player {
         y += newY;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public int getX() {
+        return x;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public int getY() {
+        return y;
     }
 }
