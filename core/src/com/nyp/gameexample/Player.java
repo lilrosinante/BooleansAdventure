ac250c81e294ebfd03e880a7383ce1ce9c42b8f1
lilrosinante <60463014+lilrosinante@users.noreply.github.com>
2021-06-03 09:11:16 +0200

    TileMap created

--- core/src/com/nyp/gameexample/Player.java
+++ core/src/com/nyp/gameexample/Player.java
@@ -1,18 +1,37 @@
 package com.nyp.gameexample;
 
+import com.nyp.gameexample.model.TileMap;
+
 public class Player {
 
+    private TileMap map;
     private int x;
     private int y;
 
-    public Player(int x, int y) {
+    public Player(TileMap map, int x, int y) {
+        this.map = map;
         this.x = x;
         this.y = y;
+        map.getTile(x, y).setPlayer(this);
     }
 
-    public void move(int newX, int newY){
+    public boolean move(int newX, int newY) {
+        if (x + newX >= map.getWidth() || x + newX < 0 || y + newY >= map.getHeight() || y + newY < 0) {
+            return false;
+        }
+
+        if (map.getTile(x + newX, y + newY).getPlayer() != null) {
+            return false;
+        }
+        map.getTile(x, y).setPlayer(null);
         x += newX;
         y += newY;
+        map.getTile(x, y).setPlayer(this);
+        return true;
+    }
+
+    public void render () {
+
     }
 
     public int getX() {
